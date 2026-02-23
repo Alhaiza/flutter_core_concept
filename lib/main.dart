7abd6d58be60889b6ef231baf65401ec64a6a83c
Alhaiza <alqadrihaiza@gmail.com>
2026-02-23 14:10:50 +0700

    List View

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,22 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  // Gunakan List untuk menyimpan data (jika ada)
+  final List<Color> myColor = [
+    Colors.red,
+    Colors.green,
+    Colors.blue,
+    Colors.yellow,
+  ];
+
+  final List<Widget> myList = List.generate(
+    100,
+    (index) => Text(
+      "${index + 1}",
+      style: TextStyle(fontSize: 20 + double.parse(index.toString())),
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
     // Widget Dasar
@@ -16,19 +32,30 @@ class MyApp extends StatelessWidget {
         // backgroundColor = Wanra Background. bisa pakai Color untuk hex bisa pakai Colors buat warna yg udah disediakan
         // backgroundColor: Color(0xffC7EABB),
         // AppBar = Header Aplikasi
-        appBar: AppBar(title: Text("My Apps")),
+        appBar: AppBar(title: Text("List View")),
+        // Generate dari myList
+        body: ListView(children: myList),
         // Bagian Utama Aplikasi
-        // Ini kalau Pakai Collumn (Kalau mau liat versi non collumn liat commit basic widget)
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Container(width: 200, height: 50, color: Color(0xffA2CB8B)),
-            Container(width: 50, height: 50, color: Color(0xffC00707)),
-            Container(width: 100, height: 50, color: Color(0xffFFAA00)),
-            Container(width: 300, height: 50, color: Color(0xff7AAACE)),
-          ],
-        ),
+        // Ini kalau Pakai List agar bisa scrollable (Kalau mau liat versi non collumn liat commit basic widget)
+        // Pakai separeted buat misahin per List
+        // body: ListView.separated(
+        //   separatorBuilder: (context, index) {
+        //     // Pakai Divider
+        //     return Divider(color: Colors.black);
+        //     // Pakai ini misahin secara eksplisit
+        //     // return Container(height: 10, color: Colors.black);
+        //   },
+        //   itemCount: myColor.length,
+        //   // Pakai Builder buat looping
+        //   itemBuilder: (contex, index) {
+        //     return Container(width: 300, height: 300, color: myColor[index]);
+        //   },
+        //   // mainAxisAlignment: MainAxisAlignment.center,
+        //   // crossAxisAlignment: CrossAxisAlignment.end,
+        //   // Ubah arah scroll pakai scrollDirection: Axis.horizontal
+        //   // scrollDirection: Axis.horizontal,
+        //   // children: myList,
+        // ),
       ),
     );
   }
