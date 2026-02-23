442a20d97e373ea9cf52a33424f81c2861919597
Alhaiza <alqadrihaiza@gmail.com>
2026-02-23 14:27:00 +0700

    List Tile

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,57 @@
 import 'package:flutter/material.dart';
 
-// Main Class
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // Gunakan List untuk menyimpan data (jika ada)
-  final List<Color> myColor = [
-    Colors.red,
-    Colors.green,
-    Colors.blue,
-    Colors.yellow,
-  ];
-
-  final List<Widget> myList = List.generate(
-    100,
-    (index) => Text(
-      "${index + 1}",
-      style: TextStyle(fontSize: 20 + double.parse(index.toString())),
-    ),
-  );
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // Widget Dasar
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      // Scaffold = Pembungkus/Container Aplikasi
       home: Scaffold(
-        // backgroundColor = Wanra Background. bisa pakai Color untuk hex bisa pakai Colors buat warna yg udah disediakan
-        // backgroundColor: Color(0xffC7EABB),
-        // AppBar = Header Aplikasi
-        appBar: AppBar(title: Text("List View")),
-        // Generate dari myList
-        body: ListView(children: myList),
-        // Bagian Utama Aplikasi
-        // Ini kalau Pakai List agar bisa scrollable (Kalau mau liat versi non collumn liat commit basic widget)
-        // Pakai separeted buat misahin per List
-        // body: ListView.separated(
-        //   separatorBuilder: (context, index) {
-        //     // Pakai Divider
-        //     return Divider(color: Colors.black);
-        //     // Pakai ini misahin secara eksplisit
-        //     // return Container(height: 10, color: Colors.black);
-        //   },
-        //   itemCount: myColor.length,
-        //   // Pakai Builder buat looping
-        //   itemBuilder: (contex, index) {
-        //     return Container(width: 300, height: 300, color: myColor[index]);
-        //   },
-        //   // mainAxisAlignment: MainAxisAlignment.center,
-        //   // crossAxisAlignment: CrossAxisAlignment.end,
-        //   // Ubah arah scroll pakai scrollDirection: Axis.horizontal
-        //   // scrollDirection: Axis.horizontal,
-        //   // children: myList,
-        // ),
+        appBar: AppBar(title: Text("List Tile")),
+        body: ListView(
+          children: [
+            // Biar kayak Whatsapp/Aplikasi Chat lain
+            ListTile(
+              contentPadding: EdgeInsets.all(10),
+              title: Text("Syarif Muhammad Alhaiza"),
+              subtitle: Text(
+                "Selamat Pagishfheqfheqbvjebjbvjkebgjbekjbgjebqfbdbgjdbgjbdjgbjdgbjdgbjbgdjbfsbjfbjsbsbbgjbjbjgbjbgjbdjgb",
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+              ),
+              leading: CircleAvatar(),
+              trailing: Text("04:20 PM"),
+              tileColor: Colors.amber,
+              dense: true,
+            ),
+            Divider(color: Colors.black),
+            ListTile(
+              title: Text("Syarif Muhammad Alhaiza"),
+              subtitle: Text("Selamat Pagi"),
+              leading: CircleAvatar(),
+              trailing: Text("04:20 PM"),
+            ),
+            Divider(color: Colors.black),
+            ListTile(
+              title: Text("Syarif Muhammad Alhaiza"),
+              subtitle: Text("Selamat Pagi"),
+              leading: CircleAvatar(),
+              trailing: Text("04:20 PM"),
+            ),
+            Divider(color: Colors.black),
+            ListTile(
+              title: Text("Syarif Muhammad Alhaiza"),
+              subtitle: Text("Selamat Pagi"),
+              leading: CircleAvatar(),
+              trailing: Text("04:20 PM"),
+            ),
+            Divider(color: Colors.black),
+          ],
+        ),
       ),
     );
   }
