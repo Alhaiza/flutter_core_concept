1116b5b672ea44298472c5762b3a77d77faa8b9c
Alhaiza <alqadrihaiza@gmail.com>
2026-02-26 11:03:59 +0700

    Extract Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:faker/faker.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  // const MyApp({super.key});
+  // var faker = Faker();
 
   @override
   Widget build(BuildContext context) {
+    var faker = Faker();
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Image Widget")),
-        body: Center(
-          child: Container(
-            width: 350,
-            height: 500,
-            color: Colors.amber,
-            child: Image(
-              fit: BoxFit.cover,
-              image: AssetImage("images/image-example.jpg"),
-            ),
+        appBar: AppBar(
+          title: Text(
+            "Extract Widget",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
           ),
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: ListView.builder(
+          itemCount: 100,
+          itemBuilder: (context, index) {
+            return ChatItem(
+              imageUrl: "https://picsum.photos/id/${index + 1}/200/300",
+              title: faker.person.name(),
+              subtitle: faker.lorem.sentence(),
+            );
+          },
         ),
       ),
+    );
+  }
+}
+
+class ChatItem extends StatelessWidget {
+  final String imageUrl;
+  final String title;
+  final String subtitle;
+
+  const ChatItem({
+    super.key,
+    required this.imageUrl,
+    required this.title,
+    required this.subtitle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundColor: Colors.blueAccent,
+        backgroundImage: NetworkImage(imageUrl),
+      ),
+      title: Text(title),
+      subtitle: Text(subtitle),
+      trailing: Text("10:00 AM"),
     );
   }
 }
