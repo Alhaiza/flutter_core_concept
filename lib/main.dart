90d032063eebe04d3df1be4fba217098c91fa6b5
Alhaiza <alqadrihaiza@gmail.com>
2026-02-26 16:17:32 +0700

    TabBar Widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,23 +10,40 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<Tab> myTab = [
+      Tab(text: "Tab 1"),
+      Tab(text: "Tab 2"),
+      Tab(text: "Tab 3"),
+    ];
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.green,
-          leading: Container(color: Colors.greenAccent),
-          title: Container(width: 1000, height: 10, color: Colors.red),
-          centerTitle: true,
-          actions: [
-            Container(height: 35, width: 35, color: Colors.purple),
-            Container(height: 35, width: 35, color: Colors.yellow),
-          ],
-          bottom: PreferredSize(
-            preferredSize: Size.fromHeight(200),
-            child: Container(height: 150, width: 100, color: Colors.orange),
+      home: DefaultTabController(
+        length: myTab.length,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Tab Bar Widget"),
+            bottom: TabBar(
+              labelColor: Colors.deepOrange,
+              labelStyle: TextStyle(fontWeight: FontWeight.bold),
+              unselectedLabelColor: Colors.black,
+              unselectedLabelStyle: TextStyle(fontWeight: FontWeight.normal),
+              indicator: BoxDecoration(
+                // color: Colors.amber,
+                // borderRadius: BorderRadius.circular(50),
+                border: Border(
+                  bottom: BorderSide(color: Colors.deepOrange, width: 5),
+                ),
+              ),
+
+              tabs: myTab,
+            ),
           ),
-          flexibleSpace: Container(height: 350, color: Colors.deepPurple),
+          body: TabBarView(
+            children: [
+              Center(child: Text("Tab 1", style: TextStyle(fontSize: 40))),
+              Center(child: Text("Tab 2", style: TextStyle(fontSize: 40))),
+              Center(child: Text("Tab 3", style: TextStyle(fontSize: 40))),
+            ],
+          ),
         ),
       ),
     );
