ba16928aa0712345eb35ab1cd72c45ff7700f760
Alhaiza <alqadrihaiza@gmail.com>
2026-02-26 11:23:33 +0700

    Stateful Widger

--- lib/main.dart
+++ lib/main.dart
@@ -5,59 +5,56 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // const MyApp({super.key});
-  // var faker = Faker();
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    var faker = Faker();
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Extract Widget",
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-          ),
-          backgroundColor: Colors.blueAccent,
-        ),
-        body: ListView.builder(
-          itemCount: 100,
-          itemBuilder: (context, index) {
-            return ChatItem(
-              imageUrl: "https://picsum.photos/id/${index + 1}/200/300",
-              title: faker.person.name(),
-              subtitle: faker.lorem.sentence(),
-            );
-          },
-        ),
-      ),
-    );
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
-class ChatItem extends StatelessWidget {
-  final String imageUrl;
-  final String title;
-  final String subtitle;
-
-  const ChatItem({
-    super.key,
-    required this.imageUrl,
-    required this.title,
-    required this.subtitle,
-  });
-
+class _MyAppState extends State<MyApp> {
+  int counter = 1;
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: CircleAvatar(
-        backgroundColor: Colors.blueAccent,
-        backgroundImage: NetworkImage(imageUrl),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Dynamic Apps")),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              counter.toString(),
+              style: TextStyle(fontSize: 20 + double.parse(counter.toString())),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                // Elevetd button tuh buat tombol yang manggil yang bisa ngerubah sesuatu
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      counter++;
+                    });
+                    print(counter);
+                  },
+                  child: Icon(Icons.add),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      if (counter != 1) {
+                        counter--;
+                      }
+                    });
+                    print(counter);
+                  },
+                  child: Icon(Icons.remove),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
-      title: Text(title),
-      subtitle: Text(subtitle),
-      trailing: Text("10:00 AM"),
     );
   }
 }
